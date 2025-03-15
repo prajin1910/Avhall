2b775f13b84b7b04fbed1373c1956cfbd75118e4
Prajin <prajinkumar2020@gmail.com>
2025-03-15 19:54:07 +0530

    first1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const app = express();
 const port = 3000;
 
